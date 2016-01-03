a6ea62ccb4ae154d39422c4da4086731cf4461d1
Ali Saadati <AliSaadati619@yahoo.com>
2016-01-02 21:52:49 -0800

    JavaOCR
    Java Optical Character Recognition application
    Created by William Whitney

---
+++ Constants.java
@@ -0,0 +1,25 @@
+// Constants.java
+// Copyright (c) 2010 William Whitney
+// All rights reserved.
+// This software is released under the BSD license.
+// Please see the accompanying LICENSE.txt for details.
+package net.sourceforge.javaocr;
+
+import java.util.logging.Logger;
+
+/**
+ * Contains constants unique to entire application.
+ * 
+ * contains program version to be displayed within the application.
+ * @author William Whitney
+ */
+public class Constants
+{
+
+    public Constants()
+    {
+        
+    }
+    public static final String version = "1.101";
+    private static final Logger LOG = Logger.getLogger(Constants.class.getName());
+}
